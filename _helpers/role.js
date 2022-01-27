eab3a8af60515ef88a9a427a55675f80d4044790
sreeragps <sreerag@brainchild.services>
2022-01-27 22:22:40 +0530

    node_assignment_completion_part_1

---
+++ _helpers/role.js
@@ -0,0 +1,6 @@
+module.exports = {
+    Admin: 'Admin',
+    Customer: 'Customer',
+    Moderator: 'Moderator'
+
+}
---
+++ users/user.service.js
@@ -0,0 +1,55 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const Role = require('_helpers/role');
+
+// users hardcoded for simplicity,will be in db
+const users = [
+    { id: 1, username: 'admin', password: 'admin', firstName: 'Admin', lastName: 'User', role: Role.Admin },
+    { id: 2, username: 'customer', password: 'customer', firstName: 'customer', lastName: 'User', role: Role.Customer },
+    { id: 3, username: 'moderator', password: 'moderator', firstName: 'moderator', lastName: 'User', role: Role.Admin }
+];
+
+module.exports = {
+    authenticate,
+    getAll,
+    getById,
+    getByType
+};
+
+async function authenticate({ username, password }) {
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+        const token = jwt.sign({ sub: user.id, role: user.role }, config.secret);
+        const { password, ...userWithoutPassword } = user;
+        return {
+            ...userWithoutPassword,
+            token
+        };
+    }
+}
+
+async function getAll() {
+    return users.map(u => {
+        const { password, ...userWithoutPassword } = u;
+        return userWithoutPassword;
+    });
+}
+
+async function getById(id) {
+    const user = users.find(u => u.id === parseInt(id));
+    if (!user) return;
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+}
+
+async function getByType(type) {
+    const userModerator = users.map(u =>{
+
+        if(u.type==type){
+            const { password, ...userWithoutPassword } = u;
+            return userWithoutPassword;
+        }
+    } )
+  
+   
+}
